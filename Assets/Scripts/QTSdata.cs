3d2f6235cf38b82ed93c5d2933dc5f264910039a
ernar <aubakirernar111@gmail.com>
2025-04-28 03:13:28 +0500

    adding a word game and true false questions

--- Assets/Scripts/QTSdata.cs
+++ Assets/Scripts/QTSdata.cs
@@ -3,12 +3,22 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New QuestionData", menuName = "QuestionData")]
 public class QtsData : ScriptableObject
 {
-    [System.Serializable]
-    public struct Question
+    public enum QuestionType
     {
+        MultipleChoice, // 4 варианта
+        TrueFalse,       // True/False
+        WordGame 
+    }
+
+    [System.Serializable]
+    public class Question
+    {
+        public QuestionType questionType; // Тип вопроса
         public string questionText;
-        public string[] replies;
-        public int correctReplyIndex;
+        public string[] replies;          // Только для MultipleChoice
+        public int correctReplyIndex;     // Только для MultipleChoice
+        public bool correctTrueFalse;     // Только для TrueFalse
+        public string correctWordAnswer; // WordGame
     }
 
     public Question[] questions;
--- Assets/Scripts/QuestionManager.cs
+++ Assets/Scripts/QuestionManager.cs
@@ -1,18 +1,21 @@
-using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
-
+using TMPro;
 public class QuestionManager : MonoBehaviour
 {
     public Text questionText;
     public Text scoreText;
     public Text FinalScore;
     public Button[] replyButtons;
-    public QtsData qtsData;
+    public QtsData qtsData; // Reference to the scriptable object
     public GameObject Right;
     public GameObject Wrong;
     public GameObject GameFinished;
+
+    public TMP_InputField answerInputField; // ⚡ Подключи InputField сюда через инспектор
+    public Button submitButton; // Кнопка "Подтвердить ответ"
+
 
     private int currentQuestion = 0;
     private static int score = 0;
@@ -26,45 +29,103 @@ public class QuestionManager : MonoBehaviour
     }
 
     void SetQuestion(int questionIndex)
+{
+    var question = qtsData.questions[questionIndex];
+    questionText.text = question.questionText;
+
+    // Remove previous listeners
+    foreach (Button r in replyButtons)
     {
-        questionText.text = qtsData.questions[questionIndex].questionText;
+        r.onClick.RemoveAllListeners();
+    }
 
-        foreach (Button r in replyButtons)
-        {
-            r.onClick.RemoveAllListeners();
-        }
+    answerInputField.gameObject.SetActive(false);
+    submitButton.gameObject.SetActive(false);
 
+    if (question.questionType == QtsData.QuestionType.MultipleChoice)
+    {
         for (int i = 0; i < replyButtons.Length; i++)
         {
-            replyButtons[i].GetComponentInChildren<Text>().text = qtsData.questions[questionIndex].replies[i];
+            replyButtons[i].gameObject.SetActive(true);
+            replyButtons[i].GetComponentInChildren<Text>().text = question.replies[i];
             int replyIndex = i;
-            replyButtons[i].onClick.AddListener(() => 
+            replyButtons[i].onClick.AddListener(() =>
             {
                 CheckReply(replyIndex);
             });
         }
     }
-
-    void CheckReply(int replyIndex)
+    else if (question.questionType == QtsData.QuestionType.TrueFalse)
     {
-        if (replyIndex == qtsData.questions[currentQuestion].correctReplyIndex)
-        {
-            score++;
-            scoreText.text = "" + score;
-            Right.gameObject.SetActive(true);
-        }
-        else
-        {
-            Wrong.gameObject.SetActive(true);
-        }
+        replyButtons[0].gameObject.SetActive(true);
+        replyButtons[1].gameObject.SetActive(true);
+        replyButtons[2].gameObject.SetActive(false);
+        replyButtons[3].gameObject.SetActive(false);
 
+        replyButtons[0].GetComponentInChildren<Text>().text = "True";
+        replyButtons[1].GetComponentInChildren<Text>().text = "False";
+
+        replyButtons[0].onClick.AddListener(() => CheckTrueFalse(true));
+        replyButtons[1].onClick.AddListener(() => CheckTrueFalse(false));
+    }
+    else if (question.questionType == QtsData.QuestionType.WordGame)
+    {
         foreach (Button r in replyButtons)
         {
-            r.interactable = false;
+            r.gameObject.SetActive(false);
         }
 
-        StartCoroutine(Next());
+        answerInputField.gameObject.SetActive(true);
+        submitButton.gameObject.SetActive(true);
+
+        submitButton.onClick.RemoveAllListeners();
+        submitButton.onClick.AddListener(CheckWordAnswer);
     }
+}
+
+
+    void CheckReply(int replyIndex)
+{
+    if (replyIndex == qtsData.questions[currentQuestion].correctReplyIndex)
+    {
+        score++;
+        scoreText.text = "" + score;
+        Right.gameObject.SetActive(true);
+    }
+    else
+    {
+        Wrong.gameObject.SetActive(true);
+    }
+
+    foreach (Button r in replyButtons)
+    {
+        r.interactable = false;
+    }
+
+    StartCoroutine(Next());
+}
+
+    void CheckTrueFalse(bool playerAnswer)
+{
+    if (playerAnswer == qtsData.questions[currentQuestion].correctTrueFalse)
+    {
+        score++;
+        scoreText.text = "" + score;
+        Right.gameObject.SetActive(true);
+    }
+    else
+    {
+        Wrong.gameObject.SetActive(true);
+    }
+
+    foreach (Button r in replyButtons)
+    {
+        r.interactable = false;
+    }
+
+    StartCoroutine(Next());
+}
+
 
     IEnumerator Next()
     {
@@ -74,12 +135,15 @@ public class QuestionManager : MonoBehaviour
 
         if (currentQuestion < qtsData.questions.Length)
         {
+            // Reset for the next question
             Reset();
         }
         else
         {
+            // Game finished
             GameFinished.SetActive(true);
-            float scorePercentage = (float)score / qtsData.questions.Length * 100;
+
+            float scorePercentage = (float)score / qtsData.questions.Length * 100f;
             FinalScore.text = "You scored " + scorePercentage.ToString("F0") + "%";
 
             if (scorePercentage < 50)
@@ -107,14 +171,45 @@ public class QuestionManager : MonoBehaviour
 
     public void Reset()
     {
+        // Hide feedback panels
         Right.SetActive(false);
         Wrong.SetActive(false);
 
+        // Enable all reply buttons
         foreach (Button r in replyButtons)
         {
             r.interactable = true;
         }
 
+        answerInputField.text = "";
+        answerInputField.interactable = true;
+        submitButton.interactable = true;
+
+        // Set the next question
         SetQuestion(currentQuestion);
     }
+
+
+    void CheckWordAnswer()
+{
+    string playerAnswer = answerInputField.text.Trim().ToLower();
+    string correctAnswer = qtsData.questions[currentQuestion].correctWordAnswer.Trim().ToLower();
+
+    if (playerAnswer == correctAnswer)
+    {
+        score++;
+        scoreText.text = "" + score;
+        Right.gameObject.SetActive(true);
+    }
+    else
+    {
+        Wrong.gameObject.SetActive(true);
+    }
+
+    answerInputField.interactable = false;
+    submitButton.interactable = false;
+
+    StartCoroutine(Next());
 }
+}
+
